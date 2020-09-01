7249792a94fe17b56b8f766cbe79ff704d956dd7
thomas <thomaspesneau@gmail.com>
2020-09-01 10:32:17 +0200

    Export command_with_config

--- click_click/__init__.py
+++ click_click/__init__.py
@@ -1,5 +1,1 @@
-"""Top-level package for double_click."""
-
-__author__ = """Thomas PESNEAU"""
-__email__ = 'thomaspesneau@gmail.com'
-__version__ = '0.1.0'
+from click_click.click_click.decorators import command_with_config
